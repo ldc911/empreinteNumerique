0244cddf7281403622915a4c8c64d178f801c3ac
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-09-28 14:14:51 +0200

    Ajout de la clef de sol

--- header.js
+++ header.js
@@ -1,9 +1,19 @@
 const headerNav = document.querySelector("#headerNav");
 const headerNavLign = document.querySelectorAll(".headerNavLign");
 
+// headerNav.addEventListener('click', function () {
+//     for (let i = 0; i < headerNavLign.length; i++) {
+//         if (headerNavLign[i].style.width = "3rem") {
+//             headerNavLign[i].style.width = "89vh";
+//         } else {
+//             console.log("hello");
+//             headerNavLign[i].style.width = "3rem";
+//         }
+//     }
+// });
+
 headerNav.addEventListener('click', function () {
     for (let i = 0; i < headerNavLign.length; i++) {
-        headerNavLign[i].style.width = "89vh";
+        headerNavLign[i].classList.toggle("headerNavLignMax");
     }
 });
-
