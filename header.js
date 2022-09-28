ca235e6b480c05314039af6663fe01a94670b8b5
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-09-28 15:16:07 +0200

    Menu déroulant avec items fonctionnel (à perfectionner)

--- header.js
+++ header.js
@@ -1,5 +1,6 @@
 const headerNav = document.querySelector("#headerNav");
 const headerNavLign = document.querySelectorAll(".headerNavLign");
+const navItem = document.querySelectorAll(".navItem");
 
 // headerNav.addEventListener('click', function () {
 //     for (let i = 0; i < headerNavLign.length; i++) {
@@ -15,5 +16,8 @@ const headerNavLign = document.querySelectorAll(".headerNavLign");
 headerNav.addEventListener('click', function () {
     for (let i = 0; i < headerNavLign.length; i++) {
         headerNavLign[i].classList.toggle("headerNavLignMax");
+    }
+    for (let j = 0; j < navItem.length; j++) {
+        navItem[j].classList.toggle("navItemApparition");
     }
 });
