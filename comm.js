a63429f3c9e9881ffeb83932a5d676c8f2aa9c4d
Laurent Duclos <lduclos@protonmail.com>
2022-09-28 23:36:50 +0200

    add comm section with stars rating feature

---
+++ comm.js
@@ -0,0 +1,107 @@
+//on va chercher toutes les étoiles
+const stars = document.querySelectorAll(".emptyStar");
+//stars.style.color= "lightgray";
+//on va chercher la note, l'input
+const note = document.querySelector("#note");
+
+//on boucle sur les étoies pour ajouter un event listener
+for (star of stars) {
+    //écoute du survol
+    star.addEventListener("mouseover", function () {
+        resetStars();
+        this.classList.add("fullStar");
+        this.classList.remove("emptyStar");
+
+        //elem précédent dans le dom de même niveau ou balise soeur
+        let previousStar = this.previousElementSibling;
+
+        while (previousStar) {//tant qu'il y a une étoile précédente
+            //permutation des classes étoile pleine/étoile vide
+            previousStar.classList.add("fullStar");
+            previousStar.classList.remove("emptyStar");
+            //on récupère létoile qui précède
+            previousStar = previousStar.previousElementSibling;
+        }
+
+    });
+
+    //event listener sur le clic
+    star.addEventListener("click", function () {
+        note.value = this.dataset.value;
+    });
+
+    star.addEventListener("mouseout", function () {
+        resetStars(note.value);
+    });
+}
+
+
+
+function resetStars(note = 0) {
+    for (star of stars) {
+        if (star.dataset.value > note) {
+            star.classList.add("emptyStar");
+            star.classList.remove("fullStar");
+        } else {
+            star.classList.add("fullStar");
+            star.classList.remove("emptyStar");
+        }
+
+    }
+}
+//on récupère les éléments du form
+const commentBoxes = document.querySelector(".commentBoxes");
+
+const formButton = document.querySelector(".formButton");
+
+function createCommentBox(title, note, content, nom, mail) {
+    const commentBox = document.createElement("div");
+    commentBox.classList.add("commentBox");
+    commentBoxes.appendChild(commentBox);
+
+    const commentHeader = document.createElement("div");
+    commentHeader.classList.add("commentBoxHeader");
+    commentBox.appendChild(commentHeader);
+
+    const commentHeaderContentA = document.createElement("div");
+    commentHeaderContentA.classList.add("commentBoxHeaderContent");
+    commentHeaderContentA.innerHTML = title;
+    commentHeader.appendChild(commentHeaderContentA);
+
+    const commentHeaderContentB = document.createElement("div");
+    commentHeaderContentB.classList.add("commentBoxHeaderContent");
+    commentHeaderContentB.innerHTML = `${note}/5`;
+    commentHeader.appendChild(commentHeaderContentB);
+
+    const commentBoxContent = document.createElement("div");
+    commentBoxContent.classList.add("commentBoxContent");
+    commentBoxContent.innerHTML = content;
+    commentBox.appendChild(commentBoxContent);
+
+    const commentBoxFooter = document.createElement("div");
+    commentBoxFooter.classList.add("commentBoxFooter");
+    commentBox.appendChild(commentBoxFooter);
+
+    const footerContentA = document.createElement("div");
+    footerContentA.innerHTML = nom;
+    commentBoxFooter.appendChild(footerContentA);
+
+    const footerContentB = document.createElement("div");
+    footerContentB.innerHTML = mail;
+    commentBoxFooter.appendChild(footerContentB);
+}
+
+formButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const userName = document.getElementById("formName").value;
+
+    const formTitle = document.getElementById("formTitle").value;
+
+    const userMail = document.getElementById("formMail").value;
+
+    const note = document.getElementById("note").value;
+
+    const userComment = document.getElementById("commentaire").value;
+
+    createCommentBox(formTitle, note, userComment, userName, userMail);
+})
