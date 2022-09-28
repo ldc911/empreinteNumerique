ce0db6c877baafe2d97ab6d8c06d95db5b4b60d6
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-09-28 17:21:06 +0200

    Fixation du header sur le haut lors du scroll

--- header.js
+++ header.js
@@ -2,16 +2,6 @@ const headerNav = document.querySelector("#headerNav");
 const headerNavLign = document.querySelectorAll(".headerNavLign");
 const navItem = document.querySelectorAll(".navItem");
 
-// headerNav.addEventListener('click', function () {
-//     for (let i = 0; i < headerNavLign.length; i++) {
-//         if (headerNavLign[i].style.width = "3rem") {
-//             headerNavLign[i].style.width = "89vh";
-//         } else {
-//             console.log("hello");
-//             headerNavLign[i].style.width = "3rem";
-//         }
-//     }
-// });
 
 headerNav.addEventListener('click', function () {
     for (let i = 0; i < headerNavLign.length; i++) {
