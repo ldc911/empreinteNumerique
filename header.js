4e6529f610efdb02b1c49014851a9835cb0d5d6f
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2022-10-05 12:27:17 +0200

    arrangement du css mobile et css desktop

--- header.js
+++ header.js
@@ -1,17 +1,30 @@
 // APPARITION DU MENU AU FORMAT MOBILE
 const nav = document.querySelector("#headerNavMobile");
-const menu = document.querySelector("nav");
-nav.addEventListener('click', function () {
-    menu.classList.toggle("apparu");
-});
+const menu = document.querySelectorAll(".button");
+const main = document.querySelector("main");
+menu[0].addEventListener('click', function () {
+    menu[0].style.transform = "rotate(1turn)";
+    menu[0].style.transition = "all 1s ease";
+    main.style.filter = "blur(4px)";
+    if (nav.style.display = "none") {
+        nav.style.display = "block";
+    }
+}
+);
+
+menu[0].addEventListener('mouseleave', function () {
+    menu[0].style.transform = "rotate(-1turn)";
+    nav.style.display = "none";
+    main.style.filter = "blur(0px)";
+})
 
 //APPARITION DU MENU AU FORMAT DESKTOP
-const headerNav = document.querySelector("#headerNavDesk");
 const headerNavLign = document.querySelectorAll(".headerNavLign");
+const ligns = document.querySelector("#ligns");
 const navItem = document.querySelectorAll(".navItem");
 
 
-headerNav.addEventListener('click', function () {
+ligns.addEventListener('click', function () {
     for (let i = 0; i < headerNavLign.length; i++) {
         headerNavLign[i].classList.toggle("headerNavLignMax");
     }
