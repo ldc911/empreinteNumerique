8581d3387c407aeb8d25bba9fb71c916be8e0e1b
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2022-10-05 14:30:56 +0200

    modif photos arthur

--- expression.js
+++ expression.js
@@ -8,32 +8,32 @@ const arthurExpression = [
     {
     position : 1,
     expression : "Chanmé !",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/6_avril_2012.jpg",
     },
     {
     position : 2,
     expression : "T'es même pas un titre Freres",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/10_novembre_2016.jpg",
     },
     {
     position : 3,
     expression :"c'est pas comme ça qu'es-ce qu'on fait",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/29665294_10215624980763298_6827110433444467909_o.jpg",
     },
     {
     position : 4, 
     expression : "BOUM galipette regarde ce que je sais faire",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/Arthur_Amuseon.jpg",
     },
     {
     position : 5,
     expression :"Tu lui compresse sa race",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/im5.png",
     },
     {
     position : 6,
     expression : "canard en plastique a votre service",
-    image : "css/arthurPictures/78815489.jpeg",
+    image : "css/arthurPictures/im12.png",
     },
 ];
 
